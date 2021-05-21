cf78979ffd8eea191e0bc4c94e1bb5d75904fd7a
omiko lomsadze <omiko.lomsadze@gmail.com>
2021-05-21 17:22:17 +0400

    Added routing

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -8,7 +8,7 @@ import { User } from '../_models/User';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/'; 
+  baseUrl = 'https://localhost:44362/api/'; 
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
