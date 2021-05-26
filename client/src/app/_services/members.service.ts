f6a6915b39740209bd103b39d7ca281336f4f075
omiko lomsadze <omiko.lomsadze@gmail.com>
2021-05-26 12:38:36 +0400

    Added styles and Pages To Individual Users

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,15 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Member } from 'app/_models/member';
 import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
-
-
-const httpOptions = {
-  headers: new HttpHeaders ({
-    Authorization: 'bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
 
 @Injectable({
   providedIn: 'root'
@@ -20,10 +12,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
